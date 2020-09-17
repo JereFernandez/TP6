79d65f3958b048a841233aed3fdba54614232162
Jeremias Fernandez <jere_69@live.com>
2020-09-17 15:29:24 -0300

    Initial commit

---
+++ TP6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuiteTests test = new SuiteTests();
+
+            test.Pruebasignout();
+
+            test.Dispose();
+        }
+    }
+}
